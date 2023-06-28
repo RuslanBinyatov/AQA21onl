050d905b8e1dc0bae0e02d88444966cf5d3fcc29
Ruslan Binyatov <binyatovruslan85@gmail.com>
2023-06-28 21:45:16 +0300

    selenium_wd_hw

--- src/test/java/SmokeTestHW.java
+++ src/test/java/SmokeTestHW.java
@@ -1,6 +1,5 @@
 import factory.BrowserFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
@@ -8,8 +7,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import java.util.List;
-import static java.lang.Thread.sleep;
 
 public class SmokeTestHW {
     protected WebDriver driver;
@@ -46,6 +43,7 @@ public class SmokeTestHW {
 
         Assert.assertEquals(driver.findElement(By.id("floor_cable_power")).getText(), "67");
         Assert.assertEquals(driver.findElement(By.id("spec_floor_cable_power")).getText(), "6");
+
     }
 
     @AfterMethod
