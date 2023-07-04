391db7b952a38fe5deefb8fa6d435b1d815854de
Ruslan Binyatov <binyatovruslan85@gmail.com>
2023-07-04 12:53:26 +0300

    selenium_wd_hw

--- src/test/java/SmokeTestHW.java
+++ src/test/java/SmokeTestHW.java
@@ -7,6 +7,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import static java.lang.Thread.sleep;
 
 public class SmokeTestHW {
     protected WebDriver driver;
@@ -18,7 +19,7 @@ public class SmokeTestHW {
     }
 
     @Test
-    public void calcElectricHotFloorTest() {
+    public void calcElectricHotFloorTest() throws InterruptedException {
         driver.get("https://kermi-fko.ru/raschety/Calc-Rehau-Solelec.aspx");
         System.out.println(driver.getCurrentUrl());
         System.out.println(driver.getTitle());
@@ -29,8 +30,11 @@ public class SmokeTestHW {
         WebElement calcButton = driver.findElement(By.name("button"));
 
         widthInput.sendKeys("3");
+        sleep(2000);
         lenghtInput.sendKeys("4");
+        sleep(2000);
         lossesInput.sendKeys("120");
+        sleep(2000);
 
         WebElement selectWebElement1 = driver.findElement(By.id("room_type"));
         Select selectRoomType = new Select(selectWebElement1);
@@ -38,12 +42,18 @@ public class SmokeTestHW {
         Select selectHeatingType = new Select(selectWebElement2);
 
         selectRoomType.selectByVisibleText("Ванная");
+        sleep(2000);
         selectHeatingType.selectByValue("3");
+        sleep(2000);
 
         calcButton.click();
+        sleep(2000);
 
-        Assert.assertEquals(driver.findElement(By.id("floor_cable_power")).getText(), "67");
-        Assert.assertEquals(driver.findElement(By.id("spec_floor_cable_power")).getText(), "6");
+        WebElement resultFloorCablePower = driver.findElement(By.id("floor_cable_power"));
+        Assert.assertEquals(resultFloorCablePower.getAttribute("value"), "67");
+
+        WebElement resultSpecFloorCablePower = driver.findElement(By.id("spec_floor_cable_power"));
+        Assert.assertEquals(resultSpecFloorCablePower.getAttribute("value"),"6");
     }
 
     @AfterMethod
