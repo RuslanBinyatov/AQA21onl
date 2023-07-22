5f7807586217a64871d0cc58332a26fc12860ad9
Ruslan Binyatov <binyatovruslan85@gmail.com>
2023-07-22 21:31:37 +0300

    pageObject_hw

--- src/test/java/tests/E2ETest_HW.java
+++ src/test/java/tests/E2ETest_HW.java
@@ -9,12 +9,7 @@ public class E2ETest_HW extends BaseTest_HW {
 
     @Test
     public void E2ETest() {
-        Assert.assertTrue(
-                loginStep_HW.successLogin_HW(
-                        ReadProperties.username(), ReadProperties.password()
-                ).isPageOpened()
-
-        );
+        Assert.assertTrue(loginStep_HW.successLogin_HW(ReadProperties.username(), ReadProperties.password()).isPageOpened());
         Assert.assertTrue(productsStep_HW.addToCart_HW().isPageOpened());
         Assert.assertTrue(productsStep_HW.removeButtonIsDisplayed().isPageOpened());
         Assert.assertTrue(productsStep_HW.openCart_HW().isPageOpened());
@@ -24,6 +19,5 @@ public class E2ETest_HW extends BaseTest_HW {
         Assert.assertTrue(checkoutYourInformationStep_HW.continueCheckout().isPageOpened());
         Assert.assertTrue(checkoutOverviewStep_HW.checkProductIsInTheOverview());
         Assert.assertTrue(checkoutOverviewStep_HW.finishCheckout().isPageOpened());
-
     }
 }
